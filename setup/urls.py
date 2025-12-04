9feeddf62f9299c836f738694216823b3859e39f
Juan Borges <juanborgesdev@gmail.com>
2025-12-04 17:52:51 -0300

    FIX: Correção final do caminho de template do Password Reset

--- setup/urls.py
+++ setup/urls.py
@@ -4,8 +4,6 @@ from django.contrib.auth import views as auth_views
 from treinos import views as treino_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('treinos.urls')),
     
     path('cadastro/', treino_views.cadastro, name='cadastro'),
 
@@ -13,17 +11,18 @@ urlpatterns = [
 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     
-    path('admin/', admin.site.urls),
-    
     path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='treinos/registration/password_reset_form.html'), name='password_reset'),
+        template_name='registration/password_reset_form.html'), name='password_reset'),
     
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='treinos/registration/password_reset_done.html'), name='password_reset_done'),
+        template_name='registration/password_reset_done.html'), name='password_reset_done'),
     
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='treinos/registration/password_reset_confirm.html'), name='password_reset_confirm'),
+        template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='treinos/registration/password_reset_complete.html'), name='password_reset_complete'),
+        template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    
+    path('admin/', admin.site.urls), 
+    path('', include('treinos.urls')),
 ]
